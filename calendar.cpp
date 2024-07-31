0e70b172321d427f3b2f856deedc82351fb2493f
uggerus <uggerus@protonmail.com>
2024-07-31 16:07:11 -0500

    Added names of months and days in the shire

---
+++ calendar.cpp
@@ -0,0 +1,7 @@
+#include <array>
+#include <string_view>
+
+
+constexpr std::array<std::string_view, 7> shire_day_names {"Sterday", "Sunday", "Monday", "Trewsday", "Hevensday", "Mersday", "Highday"};
+
+constexpr std::array<std::string_view, 12> shire_month_names {"Afteryule", "Solmath", "Rethe", "Astron", "Thrimidge", "Forelithe", "Afterlithe", "Wedmath", "Halimath", "Winterfilth", "Blotmath", "Foreyule"};
